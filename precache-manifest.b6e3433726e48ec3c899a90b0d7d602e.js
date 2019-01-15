2f6ce87b99615b408a6c2a8315165c167c582dde
Melissa Morel <mmorel@groupninemedia.com>
2019-01-15 16:40:55 -0500

    Updates

--- precache-manifest.b6e3433726e48ec3c899a90b0d7d602e.js
+++ precache-manifest.b6e3433726e48ec3c899a90b0d7d602e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
   },
   {
-    "revision": "3ec3d59d7a6f55859b5a",
-    "url": "/React-Portfolio/static/js/main.3ec3d59d.chunk.js"
+    "revision": "d52cd2a5475d88fe5b06",
+    "url": "/React-Portfolio/static/js/main.d52cd2a5.chunk.js"
   },
   {
     "revision": "f739feb7d61c48a46bed",
     "url": "/React-Portfolio/static/js/1.f739feb7.chunk.js"
   },
   {
-    "revision": "3ec3d59d7a6f55859b5a",
+    "revision": "d52cd2a5475d88fe5b06",
     "url": "/React-Portfolio/static/css/main.a991485d.chunk.css"
   },
   {
-    "revision": "46d5b5c3ca824338319fdfef25629741",
+    "revision": "ee7d171e63ab660886e5d401195adb46",
     "url": "/React-Portfolio/index.html"
   }
 ];
